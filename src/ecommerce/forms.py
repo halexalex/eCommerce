8e987285ebce02abc88696e836c3064ff098a701
Alex Harlamov <halex.alex@yandex.ru>
2017-12-07 20:45:37 +0300

    Initial commit

---
+++ src/ecommerce/forms.py
@@ -0,0 +1,74 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class ContactForm(forms.Form):
+    fullname = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'class': 'form-control',
+                   'placeholder': 'Your full name',
+                   }
+        )
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={'class': 'form-control',
+                   'placeholder': 'Your email',
+                   }
+        )
+    )
+    content = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'class': 'form-control',
+                   'placeholder': 'Your message',
+                   }
+        )
+    )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if 'gmail.com' not in email:
+            raise forms.ValidationError('Email has to be gmail.com')
+        return email
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField()
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={'class': 'form-control',
+                   'placeholder': 'Your email',
+                   }
+        )
+    )
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirm = forms.CharField(widget=forms.PasswordInput, label='Confirm password')
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        queryset = User.objects.filter(username=username)
+        if queryset.exists():
+            raise forms.ValidationError('username is taken.')
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        queryset = User.objects.filter(email=email)
+        if queryset.exists():
+            raise forms.ValidationError('email is taken.')
+        return email
+
+    def clean(self):
+        data = self.cleaned_data
+        password = self.cleaned_data.get('password')
+        password_confirm = self.cleaned_data.get('password_confirm')
+        if password_confirm != password:
+            raise forms.ValidationError('Passwords must match!')
+        return data
--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, get_user_model
+from django.shortcuts import render, redirect
+
+from .forms import ContactForm, LoginForm, RegisterForm
 
 
 def home_page(request):
@@ -7,6 +9,8 @@ def home_page(request):
         'title': 'Hello World!',
         'content': 'Welcome to the homepage',
     }
+    if request.user.is_authenticated():
+        context['premium_content'] = 'YEAHHHHHH!'
     return render(request, 'home_page.html', context)
 
 
@@ -19,10 +23,58 @@ def about_page(request):
 
 
 def contact_page(request):
+    form = ContactForm(request.POST or None)
     context = {
         'title': 'Contact page',
         'content': 'Welcome to the contact page',
+        'form': form,
     }
-    return render(request, 'home_page.html', context)
+    if form.is_valid():
+        print(form.cleaned_data)
+    # if request.method == 'POST':
+    #     # print(request.POST)
+    #     print(request.POST.get('fullname'))
+    #     print(request.POST.get('email'))
+    #     print(request.POST.get('content'))
+    return render(request, 'contact/view.html', context)
 
 
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        'form': form
+    }
+    print('User logged in')
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            # context['forms'] = LoginForm()
+            return redirect('/')
+        else:
+            print('Error')
+
+    return render(request, 'auth/login.html', context)
+
+
+User = get_user_model()
+
+
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = {
+        'form': form
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        new_user = User.objects.create_user(username, email, password)
+        print(new_user)
+
+    return render(request, 'auth/register.html', context)
