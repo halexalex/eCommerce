14b366c5f62fd60b8be58791c780f5332681ff01
Alex Harlamov <halex.alex@yandex.ru>
2017-12-23 12:00:11 +0300

    Update url names

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -13,11 +13,11 @@ from .views import (about_page, contact_page, home_page, login_page,
 
 
 urlpatterns = [
-    url(r'^$', home_page),
-    url(r'^about/$', about_page),
-    url(r'^contact/$', contact_page),
-    url(r'^login/$', login_page),
-    url(r'^register/$', register_page),
+    url(r'^$', home_page, name='home'),
+    url(r'^about/$', about_page, name='about'),
+    url(r'^contact/$', contact_page, name='contact'),
+    url(r'^login/$', login_page, name='login'),
+    url(r'^register/$', register_page, name='register'),
     url(r'^products/', include('products.urls', namespace='products')),
     # url(r'^products/$', ProductListView.as_view()),
     # url(r'^featured/$', ProductFeaturedListView.as_view()),
