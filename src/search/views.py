982624787809a00b31784002d7df20f3466f1db3
Alex Harlamov <halex.alex@yandex.ru>
2018-01-06 13:17:56 +0300

    Initial commit

--- src/search/views.py
+++ src/search/views.py
@@ -5,7 +5,14 @@ from products.models import Product
 
 
 class SearchProductView(ListView):
-    template_name = 'products/list.html'
+    template_name = 'search/view.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(SearchProductView, self).get_context_data(*args, **kwargs)
+        query = self.request.GET.get('q')
+        context['query'] = query
+        # SearchQuery.objects.create(query=query)
+        return context
 
     def get_queryset(self, *args, **kwargs):
         request = self.request
