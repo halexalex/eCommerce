bd87297b5c1d86e295a648cbea01299bfacb60bc
Alex Harlamov <halex.alex@yandex.ru>
2018-01-10 16:16:18 +0300

    Update cart view

--- src/carts/views.py
+++ src/carts/views.py
@@ -7,23 +7,21 @@ from .models import Cart
 
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
-    products = cart_obj.products.all()
-    total = 0
-    for x in products:
-        total += x.price
-    print(total)
-    cart_obj.total = total
-    cart_obj.save()
-    return render(request, 'carts/home.html', {})
+    return render(request, 'carts/home.html', {'cart': cart_obj})
 
 
 def cart_update(request):
-    print(request.POST)
-    product_id = 3  # request.POST.get('product_id')
-    product_obj = Product.objects.get(id=product_id)
-    cart_obj, new_obj = Cart.objects.new_or_get(request)
-    if product_obj in cart_obj.products.all():
-        cart_obj.products.remove(product_obj)
-    else:
-        cart_obj.products.add(product_obj)
+    product_id = request.POST.get('product_id')
+    if product_id is not None:
+        try:
+            product_obj = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            print('Show message to user, product is gone')
+            return redirect('cart:home')
+        cart_obj, new_obj = Cart.objects.new_or_get(request)
+        if product_obj in cart_obj.products.all():
+            cart_obj.products.remove(product_obj)
+        else:
+            cart_obj.products.add(product_obj)
+        request.session['cart_items'] = cart_obj.products.count()
     return redirect('cart:home')
