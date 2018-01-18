f882bb5bf091c376487204e042529e5e5db139a3
Alex Harlamov <halex.alex@yandex.ru>
2018-01-18 13:29:30 +0300

    Add minor fixes

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 
+from orders.models import Order
 from products.models import Product
 
 from .models import Cart
@@ -25,3 +26,14 @@ def cart_update(request):
             cart_obj.products.add(product_obj)
         request.session['cart_items'] = cart_obj.products.count()
     return redirect('cart:home')
+
+
+def checkout_home(request):
+    cart_obj, cart_created = Cart.objects.new_or_get(request)
+    order_obj = None
+    if cart_created or cart_obj.products.count() == 0:
+        return redirect('cart:home')
+    else:
+        order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
+    return render(request, 'carts/checkout.html',  {'object': order_obj})
+
