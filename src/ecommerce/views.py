ad3dcefcebaf0b80d8e961938f72917dad249742
Alex Harlamov <halex.alex@yandex.ru>
2018-01-18 16:14:43 +0300

    Cleanup code

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,7 +1,6 @@
-from django.contrib.auth import authenticate, get_user_model, login
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 
-from .forms import ContactForm, LoginForm, RegisterForm
+from .forms import ContactForm
 
 
 def home_page(request):
@@ -31,50 +30,4 @@ def contact_page(request):
     }
     if form.is_valid():
         print(form.cleaned_data)
-    # if request.method == 'POST':
-    #     # print(request.POST)
-    #     print(request.POST.get('fullname'))
-    #     print(request.POST.get('email'))
-    #     print(request.POST.get('content'))
     return render(request, 'contact/view.html', context)
-
-
-def login_page(request):
-    form = LoginForm(request.POST or None)
-    context = {
-        'form': form
-    }
-    print('User logged in')
-    if form.is_valid():
-        print(form.cleaned_data)
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password')
-        user = authenticate(request, username=username, password=password)
-        print(user)
-        if user is not None:
-            login(request, user)
-            # context['forms'] = LoginForm()
-            return redirect('/')
-        else:
-            print('Error')
-
-    return render(request, 'auth/login.html', context)
-
-
-User = get_user_model()
-
-
-def register_page(request):
-    form = RegisterForm(request.POST or None)
-    context = {
-        'form': form
-    }
-    if form.is_valid():
-        print(form.cleaned_data)
-        username = form.cleaned_data.get('username')
-        email = form.cleaned_data.get('email')
-        password = form.cleaned_data.get('password')
-        new_user = User.objects.create_user(username, email, password)
-        print(new_user)
-
-    return render(request, 'auth/register.html', context)
