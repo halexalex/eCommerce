b5e08fa3979276273d37648a0a59b2ef04b0412f
Alex Harlamov <halex.alex@yandex.ru>
2018-01-18 13:42:45 +0300

    Update pre_save_cart_receiver

--- src/carts/models.py
+++ src/carts/models.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.conf import settings
 from django.db import models
 from django.db.models.signals import m2m_changed, post_save, pre_save
@@ -62,7 +63,7 @@ m2m_changed.connect(m2m_changed_cart_receiver, sender=Cart.products.through)
 
 def pre_save_cart_receiver(sender, instance, *args, **kwargs):
     if instance.subtotal > 0:
-        instance.total = instance.subtotal + 10  # tax, shipping etc.
+        instance.total = Decimal(instance.subtotal) * Decimal(1.08)  # 8% tax
     else:
         instance.total = 0.00
 
