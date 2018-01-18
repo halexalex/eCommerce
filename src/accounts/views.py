41fcb05e13e7d113722b30f5f1d93265914a23c1
Alex Harlamov <halex.alex@yandex.ru>
2018-01-18 16:13:09 +0300

    Initial commit

---
+++ src/accounts/views.py
@@ -0,0 +1,47 @@
+from django.contrib.auth import authenticate, get_user_model, login
+from django.shortcuts import redirect, render
+from django.utils.http import is_safe_url
+
+from .forms import LoginForm, RegisterForm
+
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        'form': form
+    }
+    next_ = request.GET.get('next')
+    next_post = request.POST.get('next')
+    redirect_path = next_ or next_post or None
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            if is_safe_url(redirect_path, request.get_host()):
+                return redirect(redirect_path)
+            else:
+                return redirect('/')
+        else:
+            # Return an 'Invalid login' error message
+            print('Error')
+
+    return render(request, 'accounts/login.html', context)
+
+
+User = get_user_model()
+
+
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = {
+        'form': form
+    }
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        new_user = User.objects.create_user(username, email, password)
+
+    return render(request, 'accounts/register.html', context)
