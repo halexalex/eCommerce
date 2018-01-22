13cc0abcb685607b7ebe681c09d7691c6d2cedd4
Alex Harlamov <halex.alex@yandex.ru>
2018-01-22 19:24:50 +0300

    Implement Address reuse for checkout

--- src/addresses/views.py
+++ src/addresses/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import redirect, render
 from django.utils.http import is_safe_url
 
 from billing.models import BillingProfile
-
 from .forms import AddressForm
+from .models import Address
 
 
 def checkout_address_create_view(request):
@@ -31,7 +31,29 @@ def checkout_address_create_view(request):
 
         if is_safe_url(redirect_path, request.get_host()):
             return redirect(redirect_path)
-        else:
-            return redirect('cart:checkout')
+
+    return redirect('cart:checkout')
+
+
+def checkout_address_reuse_view(request):
+    if request.user.is_authenticated():
+        context = {}
+        next_ = request.GET.get('next')
+        next_post = request.POST.get('next')
+        redirect_path = next_ or next_post or None
+
+        if request.method == "POST":
+            print(request.POST)
+            shipping_address = request.POST.get('shipping_address', None)
+            address_type = request.POST.get('address_type', 'shipping')
+            billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+            if shipping_address is not None:
+                qs = Address.objects.filter(billing_profile=billing_profile, id=shipping_address)
+                if qs.exists():
+                    request.session[address_type + '_address_id'] = shipping_address
+                    print(address_type + '_address_id')
+
+            if is_safe_url(redirect_path, request.get_host()):
+                return redirect(redirect_path)
 
     return redirect('cart:checkout')
