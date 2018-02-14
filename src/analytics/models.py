c5e31f03c070c6e490a85540259265ca8f6801a8
Alex Harlamov <halex.alex@yandex.ru>
2018-02-14 12:50:39 +0300

    Handle the Object Viewed Signal

--- src/analytics/models.py
+++ src/analytics/models.py
@@ -3,6 +3,9 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
+from .signals import object_viewed_signal
+from .utils import get_client_ip
+
 User = settings.AUTH_USER_MODEL
 
 
@@ -21,3 +24,17 @@ class ObjectViewed(models.Model):
         ordering = ['-timestamp']  # most recent saved show up first
         verbose_name = 'Object viewed'
         verbose_name_plural = 'Objects viewed'
+
+
+def object_viewed_receiver(sender, instance, request, *args, **kwargs):
+    c_type = ContentType.objects.get_for_model(sender)  # instance.__class__
+    new_view_obj = ObjectViewed.objects.create(
+                user=request.user,
+                content_type=c_type,
+                object_id=instance.id,
+                ip_address=get_client_ip(request)  # questionable 
+        )
+
+
+object_viewed_signal.connect(object_viewed_receiver)
+
