451659a8bf162753cd64fad724ac3f833db2cb25
Alex Harlamov <halex.alex@yandex.ru>
2018-02-14 14:27:48 +0300

    Implement handling and ending user sessions

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,10 +1,11 @@
-from django.contrib.auth import authenticate, get_user_model, login
-from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
 from django.utils.http import is_safe_url
 from django.views.generic import CreateView, FormView
 
 from .forms import GuestForm, LoginForm, RegisterForm
 from .models import GuestEmail
+from .signals import user_logged_in
 
 
 def guest_register_view(request):
@@ -42,6 +43,7 @@ class LoginView(FormView):
         user = authenticate(request, username=email, password=password)
         if user is not None:
             login(request, user)
+            user_logged_in.send(user.__class__, instance=user, request=request)
             try:
                 del request.session['guest_email_id']
             except:
@@ -53,49 +55,8 @@ class LoginView(FormView):
         return super(LoginView, self).form_invalid(form)
 
 
-# def login_page(request):
-#     form = LoginForm(request.POST or None)
-#     context = {
-#         'form': form
-#     }
-#     next_ = request.GET.get('next')
-#     next_post = request.POST.get('next')
-#     redirect_path = next_ or next_post or None
-#     if form.is_valid():
-#         username = form.cleaned_data.get('username')
-#         password = form.cleaned_data.get('password')
-#         user = authenticate(request, username=username, password=password)
-#         if user is not None:
-#             login(request, user)
-#             try:
-#                 del request.session['guest_email_id']
-#             except:
-#                 pass
-#             if is_safe_url(redirect_path, request.get_host()):
-#                 return redirect(redirect_path)
-#             else:
-#                 return redirect('/')
-#         else:
-#             # Return an 'Invalid login' error message
-#             print('Error')
-#
-#     return render(request, 'accounts/login.html', context)
-
-
 class RegisterView(CreateView):
     form_class = RegisterForm
     template_name = 'accounts/register.html'
     success_url = '/login/'
 
-# User = get_user_model()
-#
-#
-# def register_page(request):
-#     form = RegisterForm(request.POST or None)
-#     context = {
-#         'form': form
-#     }
-#     if form.is_valid():
-#         form.save()
-#
-#     return render(request, 'accounts/register.html', context)
--- src/analytics/models.py
+++ src/analytics/models.py
@@ -1,19 +1,25 @@
 from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.sessions.models import Session
 from django.db import models
+from django.db.models.signals import pre_save, post_save
 
+from accounts.signals import user_logged_in
 from .signals import object_viewed_signal
 from .utils import get_client_ip
 
 User = settings.AUTH_USER_MODEL
 
+FORCE_SESSION_TO_ONE = getattr(settings, 'FORCE_SESSION_TO_ONE', False)
+FORCE_INACTIVE_USER_ENDSESSION = getattr(settings, 'FORCE_INACTIVE_USER_ENDSESSION', False)
+
 
 class ObjectViewed(models.Model):
-    user           = models.ForeignKey(User, blank=True,  null=True)  # User instance
+    user           = models.ForeignKey(User, blank=True, null=True)  # User instance
     ip_address     = models.CharField(max_length=220, blank=True, null=True)  # IP Field
     content_type   = models.ForeignKey(ContentType)  # User, Product, Order, Cart, Address
-    object_id      = models.PositiveIntegerField()   # User id, Product id, Order id
+    object_id      = models.PositiveIntegerField()  # User id, Product id, Order id
     content_object = GenericForeignKey('content_type', 'object_id')  # Product instance
     timestamp      = models.DateTimeField(auto_now_add=True)
 
@@ -29,12 +35,72 @@ class ObjectViewed(models.Model):
 def object_viewed_receiver(sender, instance, request, *args, **kwargs):
     c_type = ContentType.objects.get_for_model(sender)  # instance.__class__
     new_view_obj = ObjectViewed.objects.create(
-                user=request.user,
-                content_type=c_type,
-                object_id=instance.id,
-                ip_address=get_client_ip(request)  # questionable 
-        )
+        user=request.user,
+        content_type=c_type,
+        object_id=instance.id,
+        ip_address=get_client_ip(request)  # questionable
+    )
 
 
 object_viewed_signal.connect(object_viewed_receiver)
 
+
+class UserSession(models.Model):
+    user        = models.ForeignKey(User, blank=True, null=True)
+    ip_address  = models.CharField(max_length=220, blank=True, null=True)
+    session_key = models.CharField(max_length=100, blank=True, null=True)  # min 50
+    timestamp   = models.DateTimeField(auto_now_add=True)
+    active      = models.BooleanField(default=True)
+    ended       = models.BooleanField(default=False)
+
+    def end_session(self):
+        session_key = self.session_key
+        ended = self.ended
+        try:
+            Session.session_key.get(pk=session_key).delete()
+            self.active = False
+            self.ended = True
+            self.save()
+        except:
+            pass
+        return self.ended
+
+
+def post_save_session_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        qs = UserSession.objects.filter(user=instance.user, ended=False, active=False).exclude(id=instance.id)
+        for i in qs:
+            i.end_session()
+    if not instance.active and not instance.ended:
+        instance.end_session()
+
+
+if FORCE_SESSION_TO_ONE:
+    post_save.connect(post_save_session_receiver, sender=UserSession)
+
+
+def post_save_user_changed_receiver(sender, instance, created, *args, **kwargs):
+    if not created:
+        if not instance.is_active:
+            qs = UserSession.objects.filter(user=instance.user, ended=False, active=False)
+            for i in qs:
+                i.end_session()
+
+
+if FORCE_INACTIVE_USER_ENDSESSION:
+    post_save.connect(post_save_user_changed_receiver, sender=UserSession)
+
+
+def user_logged_in_receiver(sender, instance, request, *args, **kwargs):
+    print(instance)
+    user = instance
+    ip_address = get_client_ip(request)
+    session_key = request.session.session_key  # Django 1.11
+    UserSession.objects.create(
+        user=user,
+        ip_address=ip_address,
+        session_key=session_key
+    )
+
+
+user_logged_in.connect(user_logged_in_receiver)
