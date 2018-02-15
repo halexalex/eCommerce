0ec6aab93be4f1f7b7e4f3e0586546073fc38ab0
Alex Harlamov <halex.alex@yandex.ru>
2018-02-15 14:13:16 +0300

    Improve Payment Method Form

--- src/billing/views.py
+++ src/billing/views.py
@@ -1,5 +1,7 @@
 from django.conf import settings
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
+from django.utils.http import is_safe_url
 
 import stripe
 
@@ -8,6 +10,15 @@ STRIPE_PUB_KEY = settings.STRIPE_PUB_KEY
 
 
 def payment_method_view(request):
-    if request.method == "POST":
+    next_url = None
+    next_ = request.GET.get('next')
+    if is_safe_url(next_, request.get_host()):
+        next_url = next_
+    return render(request, 'billing/payment-method.html', {"publish_key": STRIPE_PUB_KEY, "next_url": next_url})
+
+
+def payment_method_create_view(request):
+    if request.method == "POST" and request.is_ajax():
         print(request.POST)
-    return render(request, 'billing/payment-method.html', {'publish_key': STRIPE_PUB_KEY})
+        return JsonResponse({"message": "Done"})
+    return HttpResponse("error", status_code=401)
