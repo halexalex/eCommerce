bfa3dfd1ff2e3a81ab1f0899e93ff3b0b64b2321
Alex Harlamov <halex.alex@yandex.ru>
2018-04-03 18:48:26 +0300

    Update production settings

--- ecommerce/settings/__init__.py
+++ ecommerce/settings/__init__.py
@@ -1,12 +1,12 @@
 # from .base import *
 #
-# from .production import *
+from .production import *
 
-from .local import *
-
-try:
-    from .local import *
-except:
-    pass
+# from .local import *
+#
+# try:
+#     from .local import *
+# except:
+#     pass
 
 
