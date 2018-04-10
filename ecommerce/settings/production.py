2ee4d9e8c7b04e39d3b8923c95adf263c1766e8d
Alex Harlamov <halex.alex@yandex.ru>
2018-04-10 19:12:48 +0300

    Update environment keys for production

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -19,7 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p9osyyg2x1wa8d$tg9sq8@u@+%4pvd^6b*@9i=g1cb4==9r*zu'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -57,12 +58,12 @@ AUTH_USER_MODEL = 'accounts.User'  # changes the built in user model to ours
 FORCE_SESSION_TO_ONE = False
 FORCE_INACTIVE_USER_ENDSESSION = False
 
-MAILCHIMP_API_KEY = '12da674bf3abf0c1bd2dab5a12b76cc5-us12'
+MAILCHIMP_API_KEY = os.environ.get('MAILCHIMP_API_KEY')
 MAILCHIMP_DATA_CENTER = 'us12'
-MAILCHIMP_EMAIL_LIST_ID = 'a914bf1dd4'
+MAILCHIMP_EMAIL_LIST_ID = os.environ.get('MAILCHIMP_EMAIL_LIST_ID')
 
-STRIPE_SECRET_KEY = 'sk_test_4iOsXdUfCAWUKc9z2jgm8nMz'
-STRIPE_PUB_KEY = 'pk_test_n4x1iwLTjFybqvvA5Q9bAAib'
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', 'sk_test_4iOsXdUfCAWUKc9z2jgm8nMz')
+STRIPE_PUB_KEY = os.environ.get('STRIPE_PUB_KEY', 'pk_test_n4x1iwLTjFybqvvA5Q9bAAib')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
