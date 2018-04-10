ce9d280d8d517226ece508e4833043afa2dbbe58
Alex Harlamov <halex.alex@yandex.ru>
2018-04-10 13:50:28 +0300

    Add AWS config

---
+++ ecommerce/aws/conf.py
@@ -0,0 +1,30 @@
+import datetime
+
+AWS_USERNAME = "halex-ecommerce"
+AWS_GROUP_NAME = "ECOMMERCE_GROUP"
+AWS_ACCESS_KEY_ID = "AKIAIESIDKVO7YWHENYA"
+AWS_SECRET_ACCESS_KEY = "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"
+
+AWS_FILE_EXPIRE = 200
+AWS_PRELOAD_METADATA = True
+AWS_QUERYSTRING_AUTH = False
+
+DEFAULT_FILE_STORAGE = 'ecommerce.aws.utils.MediaRootS3BotoStorage'
+STATICFILES_STORAGE = 'ecommerce.aws.utils.StaticRootS3BotoStorage'
+AWS_STORAGE_BUCKET_NAME = 'halex-bucket'
+S3DIRECT_REGION = 'us-east-1'
+S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_ROOT = MEDIA_URL
+STATIC_URL = S3_URL + 'static/'
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
+two_months = datetime.timedelta(days=61)
+date_two_months_later = datetime.date.today() + two_months
+expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
+
+AWS_HEADERS = {
+    'Expires': expires,
+    'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()),),
+}
+
