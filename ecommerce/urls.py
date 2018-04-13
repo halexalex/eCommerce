4ee7f9162a32b2306a3eccbf68ce6097021a8a76
Alex Harlamov <halex.alex@yandex.ru>
2018-04-13 13:16:21 +0300

    Improve url naming

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import LogoutView
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView, TemplateView
 
 from accounts.views import LoginView, RegisterView, guest_register_view
 from addresses.views import (checkout_address_create_view,
@@ -17,7 +17,8 @@ from .views import about_page, contact_page, home_page
 urlpatterns = [
     url(r'^$', home_page, name='home'),
     url(r'^about/$', about_page, name='about'),
-    url(r'^accounts/$', include('accounts.urls', namespace='accounts')),
+    url(r'^accounts/$', RedirectView.as_view(url='/account')),
+    url(r'^account/$', include('accounts.urls', namespace='account')),
     url(r'^contact/$', contact_page, name='contact'),
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^checkout/address/create/$', checkout_address_create_view, name='checkout_address_create'),
@@ -29,9 +30,10 @@ urlpatterns = [
     url(r'^billing/payment-method/$', payment_method_view, name='billing-payment-method'),
     url(r'^billing/payment-method/create/$', payment_method_create_view, name='billing-payment-method-endpoint'),
     url(r'^register/$', RegisterView.as_view(), name='register'),
-    url(r'^bootstrap/$', TemplateView.as_view(template_name='bootstrap/example.html')),
+    # url(r'^bootstrap/$', TemplateView.as_view(template_name='bootstrap/example.html')),
     url(r'^products/', include('products.urls', namespace='products')),
     url(r'^search/', include('search.urls', namespace='search')),
+    url(r'^settings/$', RedirectView.as_view(url='/account')),
     url(r'^settings/email/$', MarketingPreferenceUpdateView.as_view(), name='marketing-pref'),
     url(r'^webhooks/mailchimp/$', MailChimpWebhookView.as_view(), name='webhooks-mailchimp'),
     url(r'^admin/', admin.site.urls),
