4f36364416d4ffe8571a41f853a2607607301b7c
Alex Harlamov <halex.alex@yandex.ru>
2018-04-24 16:38:47 +0300

    Add user product history view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 
+from products.views import UserProductHistoryView
 from .views import AccountEmailActivateView, AccountHomeView, UserDetailUpdateView
 
 urlpatterns = [
     url(r'^$', AccountHomeView.as_view(), name='home'),
     url(r'^details/$', UserDetailUpdateView.as_view(), name='user-update'),
+    url(r'^history/products/$', UserProductHistoryView.as_view(), name='user-product-history'),
     url(r'^email/confirm/(?P<key>[0-9A-Za-z]+)/$', AccountEmailActivateView.as_view(),
         name='email-activate'),
     url(r'^email/resend-activation/$', AccountEmailActivateView.as_view(),
--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
@@ -19,6 +20,21 @@ class ProductFeaturedListView(ListView):
 class ProductFeaturedDetailView(ObjectViewedMixin, DetailView):
     queryset = Product.objects.all().featured()
     template_name = 'products/featured-detail.html'
+
+
+class UserProductHistoryView(LoginRequiredMixin, ListView):
+    template_name = 'products/user-history.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(UserProductHistoryView, self).get_context_data(*args, **kwargs)
+        cart_obj, new_obj = Cart.objects.new_or_get(self.request)
+        context['cart'] = cart_obj
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        views = request.user.objectviewed_set.all().by_model(Product, model_queryset=True)
+        return views
 
 
 class ProductListView(ListView):
