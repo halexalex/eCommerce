9a16dec129d090be6f2a5db981299635267dd636
Alex Harlamov <halex.alex@yandex.ru>
2018-05-05 23:57:21 +0300

    Change file field storage to protected location

--- ecommerce/settings/local.py
+++ ecommerce/settings/local.py
@@ -164,6 +164,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn", "static_root")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "static_cdn", "media_root")
 
+
+PROTECTED_ROOT = os.path.join(BASE_DIR, "static_cdn", "protected_media")
+
 # Let`s Encrypt ssl/tls https
 
 CORS_REPLACE_HTTPS_REFERER = False
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -52,6 +52,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-if settings.DEBUG:
     urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
