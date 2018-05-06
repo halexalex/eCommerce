be16250a65371e4374c5bd998e5d861a5035db88
Alex Harlamov <halex.alex@yandex.ru>
2018-05-06 06:42:29 +0300

    Add download permissions check

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from mimetypes import guess_type
 from wsgiref.util import FileWrapper
 
 from django.conf import settings
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404, HttpResponse
 from django.shortcuts import render, redirect
@@ -10,6 +11,7 @@ from django.views.generic import DetailView, ListView, View
 
 from analytics.mixins import ObjectViewedMixin
 from carts.models import Cart
+from orders.models import ProductPurchase
 
 from .models import Product, ProductFile
 
@@ -91,7 +93,7 @@ class ProductDetailSlugView(ObjectViewedMixin, DetailView):
 
 
 class ProductDownloadView(View):
-    def get(self, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         slug = kwargs.get('slug')
         pk = kwargs.get('pk')
         downloads_qs = ProductFile.objects.filter(pk=pk, product__slug=slug)
@@ -99,6 +101,25 @@ class ProductDownloadView(View):
             raise Http404('Download not found!')
         download_obj = downloads_qs.first()
         # permission checks
+        can_download = False
+        user_ready = True
+        if download_obj.user_required:
+            if not request.user.is_authenticated():
+                user_ready = False
+
+        purchased_products = Product.objects.none()
+        if download_obj.free:
+            can_download = True
+            user_ready = True
+        else:
+            purchased_products = ProductPurchase.objects.products_by_request(request)
+            if download_obj.product in purchased_products:
+                can_download = True
+
+        if not can_download or not user_ready:
+            messages.error(request, 'You do not have access to download this item.')
+            return redirect(download_obj.get_default_url())
+
         file_root = settings.PROTECTED_ROOT
         filepath = download_obj.file.path
         final_filepath = os.path.join(file_root, filepath)  # where the file is stored
