1655958e0085670c31d21bec31d3bfa7e56549ed
Alex Harlamov <halex.alex@yandex.ru>
2018-05-07 18:11:23 +0300

    Add context for the order data

--- analytics/views.py
+++ analytics/views.py
@@ -2,15 +2,24 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from orders.models import Order
+
 
 class SalesView(LoginRequiredMixin, TemplateView):
     template_name = 'analytics/sales.html'
 
-    def dispatch(self, request, *args, **kwargs):
+    def dispatch(self, *args, **kwargs):
         user = self.request.user
         if not user.is_staff:
             return render(self.request, "400.html", {})
-        return super(SalesView, self).dispatch(request, *args, **kwargs)
+        return super(SalesView, self).dispatch(*args, **kwargs)
 
-    def get_context_data(self, **kwargs):
-        return super(SalesView, self).get_context_data(**kwargs)
+    def get_context_data(self, *args, **kwargs):
+        context = super(SalesView, self).get_context_data(*args, **kwargs)
+        qs = Order.objects.all()
+        print(qs)
+        context['orders'] = qs
+        context['recent_orders'] = qs.recent().not_refunded()[:5]
+        context['shipped_orders'] = qs.recent().not_refunded().by_status(status='shipped')[:5]
+        context['paid_orders'] = qs.recent().not_refunded().by_status(status='paid')[:5]
+        return context
